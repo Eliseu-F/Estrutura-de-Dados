bb76ce6c97ea7a088404b2f5503331355f239366
ELISEU FERREIRA DOS SANTOS <eliseu.fsantos1@senacsp.edu.br>
2023-08-24 22:02:12 -0300

    Estrutura de dados - atualização aula 3

--- src/main/java/ED_Aula2/Teste.java
+++ src/main/java/ED_Aula2/Teste.java
@@ -7,25 +7,31 @@ package ED_Aula2;
 public class Teste {
 
     public static void main(String[] args) throws Exception {
-        Vetor vetor = new Vetor(5);
+        VetorObjeto vetor = new VetorObjeto(5);
 
+        Contato c1 = new Contato("Claudio", "9999-2525", "claudio@gmail.com");
+        Contato c2 = new Contato("Daniel", "9999-7213", "daniel@gmail.com");
+        Contato c3 = new Contato("Miguel", "9999-6546", "miguel@gmail.com");
+        
         try {
-            for(int i=0; i <= 10; i++){
-                vetor.adiciona("elemento "+i);
-            }
             
-
+                vetor.adiciona(c1);
+                vetor.adiciona(c2);
+                vetor.adiciona(c3);
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
 
        // System.out.println(vetor.adicionaInicio(0, "elemento 0"));
         
-       vetor.remove(2);
+       // vetor.remove(2);
         
         System.out.println("Tamanho do vetor: " + vetor.tamanho());
         
-        System.out.println("Informações dentro do array: " + vetor.toString());
+        System.out.println(vetor);
+        
+       // System.out.println("Informações dentro do array: " + vetor.toString());
 
     }
 }
